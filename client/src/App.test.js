05517ede1d1f4c820c40d627e1f3edf383010da3
vidict44 <vidict44@gmail.com>
2020-07-24 19:03:49 -0500

    Example for TV

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -7,3 +7,8 @@ test('renders learn react link', () => {
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+//This a test to see if I got the branch to upload correctly
+//Ignore this message
+//Do not merge
+//This branch will most likely be deleted later
