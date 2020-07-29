41bee56f97bf3e383c3051265bea33c9209953ee
LNPappas <LPappas@gmail.com>
2020-07-29 06:56:56 -0700

    played around with JWT, was going to alter to fit message board (it calls for a req.user at the moment)

--- messageboard-server/src/middleware/jwt-verified-middleware.ts
+++ messageboard-server/src/middleware/jwt-verified-middleware.ts
@@ -5,8 +5,10 @@ import jwt from 'jsonwebtoken'
 export const JWTVerifyMiddleware = (req: any, res: Response, Next: NextFunction) => {
     try {
         let token = req.headers.authorization?.split(' ').pop()//turn the string Bearer token -> token
-        if( token){
+        if(token){
             req.user = jwt.verify(token, 'thisIsASecret')
+            console.log(`token: ${token}`);
+            
         }
         Next()
     } catch (e) {
