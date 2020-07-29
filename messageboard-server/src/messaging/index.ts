5e882e11c1036fd8c2caee8c2dd1200f21cd9510
LNPappas <LPappas@gmail.com>
2020-07-29 15:05:42 -0700

    added user service for sync messages to user db and started pub/sub method for async messages

--- messageboard-server/src/messaging/index.ts
+++ messageboard-server/src/messaging/index.ts
@@ -3,16 +3,27 @@ import { Message } from "../models/message";
 import { logger } from "../util/loggers";
 
 const {PubSub} = require('@google-cloud/pubsub');
-// // $ npm install @google-cloud/pubsub
+// $ npm install @google-cloud/pubsub
 const pubSubClient = new PubSub();
+
 const topicName = ('projects/message-board-284300/topics/message-board-topic');
 export const messageTopic = pubSubClient.topic(topicName);
-
 export async function publishMessage(message:Message){
     try {
         let messageId = await messageTopic.publishJSON(message)
         logger.debug(`Pub/Sub message ${messageId} published.`);        
     } catch (error) {
         logger.error(`Pub/Sub message error ${error}`)
+    }
+}
+
+// connect to user-server
+export const userService = pubSubClient.subscription('messageboard-service to user-service');
+export async function connectToUser(){
+    try {
+        let data = JSON.parse(Buffer.from(userService.data, 'base64').toString());
+        logger.debug(`User eamil from Pub/Sub ${data.email}`);
+    } catch (error) {
+        logger.error(error);
     }
 }
---
+++ messageboard-server/src/remote/user-service/index.ts
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+// env for host address or localhost default
+let baseURL = process.env['USER_SERVICE_HOST'] || 'http:localhost:3030'
+
+export const userServiceBaseClient = axios.create({
+    baseURL,
+    headers:{
+        'Content-Type':'application/json'
+    }
+})
