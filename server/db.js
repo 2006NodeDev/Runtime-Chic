108b35ad35b28afe7cf2c589c0b0525493f83b8c
LNPappas <LPappas@gmail.com>
2020-08-02 11:39:22 -0700

    db back to matching

--- server/db.js
+++ server/db.js
@@ -1,9 +1,9 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  host: "34.86.141.95", //"34.86.156.67",
+  host: "34.86.156.67",
   user: "postgres",
-  password: "NodeDev2006", //"password",
+  password: "password",
   port: 5432,
   database: "postgres",
 });
