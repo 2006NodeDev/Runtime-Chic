4377c1eb149502ec50fdccc31219c81cd72e0983
LNPappas <LPappas@gmail.com>
2020-08-03 08:50:34 -0700

    deployment files and Dockerfiles

--- server/db.js
+++ server/db.js
@@ -1,9 +1,9 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  host: "34.86.156.67",
-  user: "postgres",
-  password: "password",
+  host:process.env['MB_HOST'], // public ip of db instance
+  user:process.env['MB_USER'],
+  password:process.env['MB_PASSWORD'],
   port: 5432,
   database: "postgres",
 });
